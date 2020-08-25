9c1340219c366ea733f44bba82e357a78c870925
syahidnurrohim <syahidnurrohim@gmail.com>
2020-08-25 08:16:23 +0700

    commit

--- ml.py
+++ ml.py
@@ -1,4 +1,4 @@
-import math, random
+import random
 
 class Matrix:
     def __init__(self, data):
@@ -13,7 +13,7 @@ class Matrix:
                     self.matrix[i][j] += matrix[0]
                 else:
                     self.matrix[i][j] = matrix[i][j]
-        return Matrix(self.matrix)
+        return self
     
     def dot(self, matrix):
         m1_r = self.row
@@ -32,25 +32,44 @@ class Matrix:
         def s_2(i): return [self.matrix[j][i] for j in range(self.row)]
         return Matrix([s_2(i) for i in range(self.col)])
 
-    def multiply(self):
-        pass
+    def inverse(self):
+        if self.row == 2 and self.col == 2:
+            temp = self.matrix[0][0]
+            self.matrix[0][0] = self.matrix[1][1]
+            self.matrix[1][1] = temp
+            self.matrix[0][1] = -self.matrix[0][1]
+            self.matrix[1][0] = -self.matrix[1][0]
+            determinant = self.matrix[0][0]*self.matrix[1][1] - self.matrix[0][1]*self.matrix[1][0]
+            return self.multiply([1/determinant])
 
-def linear(x, bias = [1]):
-    x_p = []
-    for i, v in enumerate(x):
-        x_p.append(sum([1 + v * bias[i] for i in range(len([i]))]))
-    return x_p
+    def multiply(self, matrix):
+        for i in range(self.row):
+            for j in range(self.col):
+                if len(matrix) == 1:
+                    self.matrix[i][j] *= matrix[0]
+                else:
+                    self.matrix[i][j] = matrix[i][j]
+        return self
 
-def cost(hyp, y):
-    total = len(hyp)
-    return 1/(2*total)*sum([math.pow(hyp[i] - y[i], 2) for i in range(total)])
+def linear(x, param):
+    return [param[i][0] + sum([x[i][j] * param[i][j+1] for j in range(len(x[i]))]) for i in range(len(x))]
 
-def cost_der(hyp, y):
-    total = len(hyp)
-    return 1/total*sum([hyp[i] - y[i] for i in range(total)])
+def cost(x, y, param):
+    n = len(y)
+    x = linear(x, param)
+    return 1/(2*n)*sum([(x[i] - y[i])**2 for i in range(n)])
 
-def grad_descent(loss):
-    temp0 = loss - const
+def cost_der(x, y, param, active = 0):
+    n = len(y)
+    x = linear(x, param)
+    return 1/n*sum([x[i]*param[i][active] for i in range(n)])
+
+def grad_descent(x, y, param, lrate = 0.1, target = 0.5):
+    loss = cost_der(x, y, param, 0)
+    der = [loss - (lrate*cost_der(x, y, param, i)) for i in range(len(param[0]))]
+    print(der)
+    if target <= loss:
+        grad_descent(x, y, [der] * len(x), lrate = lrate, target = target)
 
 def normal_eq():
     pass
@@ -61,15 +80,12 @@ def main():
     # A[0][0]B[0][1] * A[0][1]B[1][1] * A[0][2]B[2][1]
     # A[0][0]B[0][2] * A[0][1]B[1][2] * A[0][2]B[2][2]
     # A[0][0]B[0][3] * A[0][1]B[1][3] * A[0][2]B[2][3]
-    
     # Linear
-    pop = [449.48, 553.57, 696.783, 870.133, 1000.4, 1309.1]
-    year = [1960, 1970, 1980, 1990, 2000, 2010]
-    A = Matrix([1] * 6, pop)
+    pop = [1,2,3,4,5,6]
+    year = [[1], [2], [3], [4], [5], [6]]
+    grad_descent(year, pop, [[1, 1]] * len(year), lrate = 0.1, target = 1)
     # Sini
-    lin = linear(pop_p)
-    loss = cost(lin, pop)
-    print(loss)
+    
 
 
 if __name__ == '__main__':
